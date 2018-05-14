5572561309b372b6cd9d41ca3e96ee8f22e6ef4c
John Okely <jlokely@gmail.com>
2018-05-14 13:46:00 +0800

    PRA-360 Fix up lint

--- src/components/Navigation/PrimaryNavigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation/PrimaryNavigation.js
@@ -6,7 +6,6 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
 import { NavLink } from 'react-router-dom'
 import onClickOutside from 'react-onclickoutside'
 import SubNavigation from '../SubNavigation'
