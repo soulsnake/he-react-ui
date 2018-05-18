cc29e1c703041ba75609025b52516f240d42f2e8
John Okely <jlokely@gmail.com>
2018-05-18 15:02:42 +0800

    Fix eslint

--- src/components/Navigation/PrimaryNavigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation/PrimaryNavigation.js
@@ -7,7 +7,6 @@
 import React, {Component, Fragment} from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import { withRouter } from 'react-router'
 import { NavLink } from 'react-router-dom'
 import onClickOutside from 'react-onclickoutside'
 import SubNavigation from '../SubNavigation'
