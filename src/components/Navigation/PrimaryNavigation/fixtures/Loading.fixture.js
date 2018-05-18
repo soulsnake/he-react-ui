9f952b935f588694a4a54a4c6655abb9651569ce
John Okely <jlokely@gmail.com>
2018-05-18 14:10:42 +0800

    Convert loading fixture to use proxies

--- src/components/Navigation/PrimaryNavigation/fixtures/Loading.fixture.js
+++ src/components/Navigation/PrimaryNavigation/fixtures/Loading.fixture.js
@@ -1,17 +1,8 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import PrimaryNavigation from '../'
 
-function PrimaryNavigationLoadingWrapped (props) {
-  const {...all} = props
-  return (
-    <BrowserRouter basePath="/loader/index.html/">
-      <PrimaryNavigation {...all} />
-    </BrowserRouter>)
-}
-
 export default {
-  component: PrimaryNavigationLoadingWrapped,
+  component: PrimaryNavigation,
+  url: '/',
   props: {
     loading: true,
     logoutRoute: '/logout',
---
+++ src/components/Navigation/SubNavigation/fixtures/loading.fixture.js
@@ -0,0 +1,14 @@
+import SubNavigation from '../'
+
+export default {
+  component: SubNavigation,
+  url: '/feedback',
+  props: {
+    item: {
+      items: []
+    },
+    locations: [].map((location) => ({value: location, label: location})),
+    loading: true,
+    logoutRoute: '/logout'
+  }
+}
