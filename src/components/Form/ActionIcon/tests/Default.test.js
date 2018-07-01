7e1dd9fd6538064c4397c0a4251c9266fccad68d
hen <hello@hen.zone>
2018-07-01 22:08:39 +0800

    Test clicking action button

--- src/components/Form/ActionIcon/tests/Default.test.js
+++ src/components/Form/ActionIcon/tests/Default.test.js
@@ -1,5 +1,14 @@
 // @flow
-import testFixture from '../../../../../config/testFixture';
+import createTestContext from 'react-cosmos-test/enzyme';
 import fixture from '../fixtures/Default.fixture.js';
 
-testFixture(fixture, 'Default');
+const { mount, getWrapper } = createTestContext({ fixture });
+
+beforeEach(mount);
+
+test('<ActionIcon /> rendered correctly with default fixture', () => {
+  const wrapper = getWrapper();
+
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('button').simulate('click');
+});
