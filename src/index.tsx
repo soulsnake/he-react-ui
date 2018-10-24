3e1015f75f8dd9f2a0c2d9e91e01d4f14ccadb78
HenG <hello@hen.zone>
2018-10-24 16:13:50 +0800

    Give all components displayNames (#176)
    After compilation as library, this should remove the curse of snapshots looking like `<t><x>...etc...</x></t>` and actually render real names like `<Popup />`

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+import * as components from './components';
+
+Object.keys(components as any).forEach(key => {
+  const item: any = (components as any)[key];
+  if (typeof item === 'function') {
+    item.displayName = key;
+  }
+});
+
 export * from './components';
